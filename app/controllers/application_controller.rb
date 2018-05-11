9ab58fa56884d5f65c8227b498cedf9ae61373b8
Trevor Seitz <trevinla@gmail.com>
2018-05-11 16:08:53 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,31 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
-    set :views, Proc.new { File.join(root, "../views/") }
+  set :views, Proc.new { File.join(root, "../views/") }
 
+  require './config/environment'
 
+  class App < Sinatra::Base
+
+    get '/' do
+      erb :"app/views/super_hero"
+    end
+
+    get '/new' do
+      erb :"app/views/super_hero"
+    end
+
+    post '/teams' do
+      # binding.pry
+      @team = Team.new(params[:team])
+
+      params[:team][:members].each do |details|
+        Superhero.new(details)
+      end
+
+      @members = Superhero.all
+
+      erb :"app/views/team"
+    end
+  end
 end
